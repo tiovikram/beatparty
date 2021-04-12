176d62f34ea195feffc5f8bd77d30eca6adbf71b
Vikram Guhan Subbiah <vsubbiah@uw.edu>
2021-04-11 17:33:18 -0700

    Spring Initializr files added

---
+++ src/main/java/com/beatparty/beatpartyapp/BeatpartyappApplication.java
@@ -0,0 +1,13 @@
+package com.beatparty.beatpartyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatpartyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatpartyappApplication.class, args);
+	}
+
+}
