d8e8cb5300f5be8dcff4a70f377af8fa46b7b71f
suyas <suyashp@uw.edu>
2021-04-27 13:28:46 -0700

    Added song layout

--- src/main/interface/src/App.tsx
+++ src/main/interface/src/App.tsx
@@ -15,15 +15,52 @@ class App extends Component<{}, AppState>{
         this.state = {
             songList: [],
         }
+        //this.getSongList();
     }
 
+    componentDidMount = () => {
+        this.getSongList();
+    }
+
+    getSongList = () => {
+        var song1 = new Song ('Bohemian Rhapsody', 'Queen', 5, 'https://open.spotify.com/album/7C2DKB8C12LqxMkfJRwTo9?highlight=spotify:track:6l8GvAyoUZwWDgF1e4822w');
+        //song.getName(); //for testing only
+        var song2 = new Song ('Thriller', 'Michael Jackson', 4, 'link here');
+        this.setState({
+            songList: [...this.state.songList, song1, song2]
+        });
+    }
+
+    goToGithub = () => {
+        window.location.href = "https://www.google.com";
+    }
+
+
+    render() {
+        const final = [];
+        for (let currentSong of this.state.songList) {
+          final.push(<button><⬆️⬆️<a href = {currentSong.getLink()}><li key={currentSong.getName()}>{currentSong.getName() + " by " + currentSong.getArtist()}</li></a>);
+        }
+        return (
+          <div className="App">
+            <TitleAndButtons/>
+            <ol>{final}</ol>
+          </div>
+        );
+      }
+
+    /*
     render(){
         return(
+            <>
             <div>
             <TitleAndButtons/>
             <p> Testing Render here</p>
+            <button onClick={this.goToGithub}>Click to go to Github</button>
             </div>
+            </>
         );
     }
+    */
 }
 export default App;
