fc40c2853829d5d4b7d9977b54e8cadafa192d82
Yasin Alissa <yasinalissa@gmail.com>
2021-04-28 13:16:50 -0700

    Fixed checkstyle-script.sh (wc should be 2, not 0)

--- checkstyle-script.sh
+++ checkstyle-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(java -jar jars/checkstyle-8.42-all.jar -c src/test/checkstyle/google_checks.xml src/main/java/ | wc -l) == 0 ]
+if [ $(java -jar jars/checkstyle-8.42-all.jar -c src/test/checkstyle/google_checks.xml src/main/java/ | wc -l) == 2 ]
 then
   exit 0
 fi
