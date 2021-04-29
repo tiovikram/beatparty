1b661bc8b5acbaf9dc774e59449d758e44697a86
Vikram Guhan Subbiah <vsubbiah@uw.edu>
2021-04-29 12:16:17 -0700

    Added 3 tests to SongControllerUnitTest.java

--- src/test/java/com/beatparty/beatpartyapp/SongControllerUnitTest.java
+++ src/test/java/com/beatparty/beatpartyapp/SongControllerUnitTest.java
@@ -26,6 +26,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 /**
  * Unit testing for SongController.java
@@ -36,6 +37,7 @@ public class SongControllerUnitTest {
     public static Random random = new Random();
     public static ObjectMapper objectMapper = new ObjectMapper();
 
+    
     @Autowired
     private SongController controller;
 
@@ -60,15 +62,20 @@ public class SongControllerUnitTest {
     @Test
     public void testGetSongs() throws JsonProcessingException {
         for (int i = 0; i < 10; i++) {
-            int numSongs = random.nextInt(5);
+            int numSongs = random.nextInt(24);
             ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/getSongs/" + numSongs, String.class);
-            Set<SongResponse> songs = objectMapper.readValue(response.getBody(), new TypeReference<Set<SongResponse>>(){ });
-            Assert.assertEquals(songs.size(), numSongs);
+
+            if (response.getStatusCodeValue() == 200) {
+                Set<SongResponse> songs = objectMapper.readValue(response.getBody(), new TypeReference<Set<SongResponse>>(){ });
+                Assert.assertEquals(numSongs, songs.size());
+            } else if (response.getStatusCodeValue() != 400) {
+                Assert.assertFalse(true);
+            }
         }
     }
 
     /**
-     *
+     * A GET call to upload a song to the backend
      */
     @Test
     public void testUploadSong() {
@@ -77,14 +84,19 @@ public class SongControllerUnitTest {
        String testUrl = "https://open.spotify.com/track/7FwBtcecmlpc1sLySPXeGE?si=0db4905b56f3419a";
        ResponseEntity<String> response = (ResponseEntity<String>) restTemplate.getForEntity("http://localhost:" + port + "/uploadNewSong/" +
                                          testName + "/" + testArtist + "/" + testUrl, String.class);
-       System.out.println(response.getStatusCode()); 
+
+       Assert.assertEquals(200, response.getStatusCodeValue());
+       Assert.assertEquals("Upload successful", response.getBody());
     }
 
     /**
-     *
+     * A GET call to delete all songs from the application
      */
     @Test
     public void testDeleteAllSongs() throws JsonProcessingException {
-	
+	ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/deleteAllSongs", String.class);
+        Assert.assertEquals(200, response.getStatusCodeValue());
+        response = restTemplate.getForEntity("http://localhost:" + port + "/getSongs/" +  1, String.class);
+        Assert.assertEquals(400, response.getStatusCodeValue());
     } 
 }
