3a606a37dd2a2813e55e844476e135d7d15aa64b
Vikram Guhan Subbiah <vsubbiah@uw.edu>
2021-05-31 21:49:16 -0700

    Changed cross origin for SongController

--- src/main/java/com/beatparty/beatpartyapp/controller/SongController.java
+++ src/main/java/com/beatparty/beatpartyapp/controller/SongController.java
@@ -24,7 +24,7 @@ import org.springframework.web.server.ResponseStatusException;
  * SongController maps http endpoints to their corresponding backend API calls.
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://beatparty.azurewebsites.net")
 public class SongController {
 
     @Autowired
